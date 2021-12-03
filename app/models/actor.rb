73cb52cb5d8b11107718ef67377dec9d745938f3
Maurice Smith <Xmaurice.smith@gmail.com>
2021-12-03 14:37:26 -0500

    commit tree 575bfd0177412306143bc5e1386546a648f66393

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.each do |c| c.show 
+        "#{character.name} - #{character.name.show}"
+    end
+  end
 end
